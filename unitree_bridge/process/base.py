3bcc0af0f6d967d34fac15cb79ea76eda2e69157
Huisouan <huishouhan@mail.ustc.edu.cn>
2025-01-02 11:37:05 +0800

    添加UnitreeBase_lab类并实现数据处理方法以支持关节角度重排序

--- unitree_bridge/process/base.py
+++ unitree_bridge/process/base.py
@@ -25,7 +25,7 @@ class UnitreeBase():
         self.robot_let_order = Botcfg.joint_order
 
         self.default_jointpos_bias = torch.tensor(Algocfg.default_jointpos_bias,device = self.device)
-    """
+
         self.bot2sim_joint_order = self.compute_index_map(self.robot_let_order, self.model_leg_order)
         self.sim2bot_joint_order = self.compute_index_map(self.model_leg_order, self.robot_let_order)
     
@@ -38,7 +38,7 @@ class UnitreeBase():
     def joint_reorder(tensor: torch.Tensor, index_map: torch.Tensor) -> torch.Tensor:
         tensor_new = tensor[index_map]
         return tensor_new
-    """
+
     def data_process(self,imu_state,motor_state):
 
         quaternion = torch.zeros(4)
@@ -65,3 +65,34 @@ class UnitreeBase():
     
     def forward(self):
         pass
+
+class UnitreeBase_lab(UnitreeBase):
+    def __init__(
+        self,
+        Algocfg:Algo_def_cfg,
+        Botcfg:Bot_def_cfg,
+        ):
+        super().__init__(Algocfg,Botcfg)
+    def data_process(self,imu_state,motor_state):
+
+        quaternion = torch.zeros(4)
+        for i in range(4):
+            quaternion[i] = imu_state.quaternion[i]
+        gyroscope = torch.zeros(3)
+        for i in range(3):
+            gyroscope[i] = imu_state.gyroscope[i]
+        joint_pos = torch.zeros(12)
+        joint_vel = torch.zeros(12)
+        for i in range(12):
+            joint_pos[i] = motor_state[i].q
+            joint_vel[i] = motor_state[i].dq
+        
+        joint_pos = joint_pos.to(self.device)
+        joint_vel = joint_vel.to(self.device)
+        gyroscope = gyroscope.to(self.device)
+        quaternion = quaternion.to(self.device)
+        # isaaclab需要对关节角度进行重排序
+        joint_pos =self.joint_reorder(joint_pos,self.bot2sim_joint_order)
+        joint_vel =self.joint_reorder(joint_vel,self.bot2sim_joint_order)
+
+        return gyroscope,quaternion,joint_pos,joint_vel
